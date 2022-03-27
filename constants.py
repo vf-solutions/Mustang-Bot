1c75a09b78640e64517697a47b9b779580f0fcda
Miguel Villa Floran <49963287+Kaweees@users.noreply.github.com>
2022-03-27 16:06:08 -0700

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,36 @@
+special_roles = {
+  'Year': {
+    "6️⃣" : "Class of 2026",
+    "5️⃣" : "Class of 2025",
+    "4️⃣" : "Class of 2024",
+    "3️⃣" : "Class of 2023"
+  },
+  'College': {
+    "🗜️" : "College of Engineering",
+    "📈" : "College of Business",
+    "🌽" : "College of Agriculture",
+    "🏗️" : "College of Architecture",
+    "📖" : "College of Liberal Arts",
+    "🔢" : "College of Math and Sci"
+  },
+  'Miscellaneous' : {
+    "🇭": "H",
+    "📦" : "Misc. Channels",
+  }
+}
+all_roles_dict = {
+  "6️⃣" : "Class of 2026",
+  "5️⃣" : "Class of 2025",
+  "4️⃣" : "Class of 2024",
+  "3️⃣" : "Class of 2023",
+  "🗜️" : "College of Engineering",
+  "📈" : "College of Business",
+  "🌽" : "College of Agriculture",
+  "🏗️" : "College of Architecture",
+  "📖" : "College of Liberal Arts",
+  "🔢" : "College of Math and Sci",
+  "🇭" : "H",
+  "📦" : "Misc. Channels",
+}
+
+all_roles_list = ["6️⃣", "5️⃣", "4️⃣", "3️⃣", "🗜️", "📈", "🌽", "🏗️", "📖", "🔢", "🇭", "📦"]
